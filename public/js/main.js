9036e4625a42cfb33b50c936c07411d87b1f9b32
christian antunes <leonidas51000@hotmail.com>
2020-11-11 19:11:16 -0300

    Refactored the code with functions and made a restart button

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,57 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
-    var conteudo = campo.val();
 
-    var qtdPalavras = conteudo.split(/\S+/).length - 1;
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
+$(document).ready(function(){ /* Calls these functions as soon as the page is loaded, you can use just $ */
+    updateSentence();
+    startCounters();
+    startChronometer();
+    $("#botao-reiniciar").click(restartGame);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
-    var cronometroID = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante <= 0){
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-})
+function updateSentence() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+
+
+function startCounters() {
+    campo.on("input", function () {
+        var conteudo = campo.val();
+
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function startChronometer() {
+
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        var cronometroID = setInterval(function () {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante <= 0) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    });
+}
+
+function restartGame() {
+        campo.attr("disabled", false);
+        campo.val("");
+        $("#contador-palavras").text("0");
+        $("#contador-caracteres").text("0");
+        $("#tempo-digitacao").text(tempoInicial);
+        startChronometer();
+}
+
+
 
